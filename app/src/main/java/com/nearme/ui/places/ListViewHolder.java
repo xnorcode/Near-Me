2835263d60fa5863988f897833ccd058af852937
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-04-11 21:37:55 +0300

    refactor: fixed ButterKnife binding

--- app/src/main/java/com/nearme/ui/places/ListViewHolder.java
+++ app/src/main/java/com/nearme/ui/places/ListViewHolder.java
@@ -31,6 +31,6 @@ public class ListViewHolder extends RecyclerView.ViewHolder {
 
     public ListViewHolder(View view) {
         super(view);
-        ButterKnife.bind(view);
+        ButterKnife.bind(this, view);
     }
 }
