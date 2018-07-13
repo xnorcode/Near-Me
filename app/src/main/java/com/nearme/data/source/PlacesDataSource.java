dd99857c30adb5303050e71f4e8a06e6f5bb3197
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-14 01:50:35 +0300

    refactor:  change Observable to Flowable

--- app/src/main/java/com/nearme/data/source/PlacesDataSource.java
+++ app/src/main/java/com/nearme/data/source/PlacesDataSource.java
@@ -5,7 +5,7 @@ import com.nearme.data.Place;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import io.reactivex.Observable;
+import io.reactivex.Flowable;
 import io.reactivex.annotations.NonNull;
 
 /**
@@ -21,7 +21,7 @@ public interface PlacesDataSource {
      * @return operation status
      * @throws IOException for network call
      */
-    Observable<Boolean> downloadAndCacheNearbyBars(@NonNull double lat, @NonNull double lng) throws IOException;
+    Flowable<Boolean> downloadAndCacheNearbyBars(@NonNull double lat, @NonNull double lng) throws IOException;
 
 
     /**
@@ -29,25 +29,25 @@ public interface PlacesDataSource {
      * @return operation status
      * @throws IOException for network call
      */
-    Observable<Boolean> searchAndCachePlace(@NonNull String name) throws IOException;
+    Flowable<Boolean> searchAndCachePlace(@NonNull String name) throws IOException;
 
 
     /**
      * @return a list of places
      */
-    Observable<ArrayList<Place>> getPlaces();
+    Flowable<ArrayList<Place>> getPlaces();
 
 
     /**
      * @param id of a place
      * @return place by id
      */
-    Observable<Place> getPlace(@NonNull String id);
+    Flowable<Place> getPlace(@NonNull String id);
 
 
     /**
      * @return operation status
      */
-    Observable<Boolean> clearCache();
+    Flowable<Boolean> clearCache();
 
 }
